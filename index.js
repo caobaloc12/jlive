56d0f24ae602de11de3b92f8418f65a7dc3214e6
loc.cao <loc.cao@grabtaxi.com>
2019-10-28 11:52:28 +0700

    Added fb login button

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('Init app')
+const express = require('express')
+const fs = require('fs')
+const app = express()
 
+app.get('/', (_req, res) => {
+  fs.readFile('./index.html', 'utf8', (err, text) => {
+      res.send(text);
+  })
+})
+
+app.listen(3000, () => {
+  console.log('App running on port 3000')
+})
